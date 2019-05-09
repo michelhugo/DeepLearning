44d0833f7d78d23d192c135eeb166b9c32ece941
Hugo Michel <hugo.michel@epfl.ch>
2019-05-09 09:47:11 +0200

    new version

---
+++ Project2/Modele_hugo.py
@@ -0,0 +1,265 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 07 08:52 2019
+
+@author: Hugo
+
+@title: Deep learning framework
+"""
+
+import numpy as np
+import torch as t
+import math
+import generate_sets_hugo as g
+
+t.set_grad_enabled(False)
+
+def sigmoid(x):
+    return 1 / (1 + (-x).exp())
+
+def dsigmoid(x):
+    return ((-x).exp())/((1+(-x).exp())**2);
+
+def relu(x):
+    return x * (x > 0).float()
+
+def drelu(x):
+    return 1. * (x > 0).float()
+
+def Tanh(x):
+    return (x.exp() - x.mul(-1).exp()) * ((x.exp() + x.mul(-1).exp()).pow(-1))
+
+def dTanh(x):
+    return 4 * (x.exp() + x.mul(-1).exp()).pow(-2)
+
+#check if loss calcul is correct -> problem of dimentsion oftherwise
+
+def MSE_loss(predictions, target):
+    return ((predictions-target)**2).mean().item()
+    
+def d_MSE_loss(predictions, target):
+    return (2*(predictions-target)).mean().item()
+
+def stable_softmax(X):
+    exps = t.exp(X - t.max(X))
+    return exps / t.sum(exps)
+
+def cross_entropy(X,y):
+    """
+    X is the output from fully connected layer (num_examples x num_classes)
+    y is labels (num_examples x 1)
+    	Note that y is not one-hot encoded vector. 
+    	It can be computed as y.argmax(axis=1) from one-hot encoded vectors of labels if required.
+    """
+    m = y.shape[0]
+    p = stable_softmax(X)
+    # We use multidimensional array indexing to extract 
+    # softmax probability of the correct label for each sample.
+    # Refer to https://docs.scipy.org/doc/numpy/user/basics.indexing.html#indexing-multi-dimensional-arrays for understanding multidimensional array indexing.
+    log_likelihood = -t.log(p[range(m),y.long()])
+    loss = t.sum(log_likelihood) / m
+    return loss
+
+def delta_cross_entropy(X,y):
+    """
+    X is the output from fully connected layer (num_examples x num_classes)
+    y is labels (num_examples x 1)
+    	Note that y is not one-hot encoded vector. 
+    	It can be computed as y.argmax(axis=1) from one-hot encoded vectors of labels if required.
+    """
+    m = y.shape[0]
+    grad = stable_softmax(X)
+    grad[range(m),y.long()] -= 1
+    grad = grad/m
+    return grad
+        
+class Module ():        
+    
+    def __init__(self):
+        self.output = None
+        self.input = None
+        
+    #*input allow arbitrary number of arguments and store them in a tuple
+    def forward (self, *input):
+        raise NotImplementedError
+        
+    #idea for backward: in each layer, stock gradients from all precedent ones -> only previous layer as argument
+    def backward (self, *input):
+        raise NotImplementedError
+    
+    #set loss from definition !
+    def set_loss (self,dl_dx):
+        self.dl_dx = dl_dx
+        
+        
+    #should return a list of pairs, each composed of a parameter tensor, and a gradient tensor
+    #of same size. This list should be empty for parameterless modules (e.g. ReLU)
+    def param ( self ):
+        return []
+    
+# --------------------------------------------------------------#
+class ReLU(Module):
+    def __init__(self):
+        super().__init__()
+        self.activation = relu
+        self.d_activation = drelu
+        
+    def forward(self, s):
+        self.input = s
+        self.output = self.activation(s)
+        
+    def backward(self, dl_dx):
+        return dl_dx*self.d_activation(self.input)
+        
+# --------------------------------------------------------------#
+class tanh(Module):
+    def __init__(self):
+        super().__init__()
+        self.activation = Tanh
+        self.d_activation = dTanh
+        
+    def forward(self, s):
+        self.input = s
+        self.output = self.activation(s)
+        
+    def backward(self, dl_dx): 
+       return dl_dx*self.d_activation(self.input)
+
+# --------------------------------------------------------------#
+class Sigmoid(Module):
+    def __init__(self):
+        super().__init__()
+        self.activation = sigmoid
+        self.d_activation = dsigmoid
+        
+    def forward(self, s):
+        self.input = s
+        self.output = self.activation(s)
+        
+    def backward(self, dl_dx): 
+       return dl_dx*self.d_activation(self.input)
+# --------------------------------------------------------------#
+class Linear(Module):
+    def __init__(self,size_in,size_out):
+        super().__init__()
+        self.init_param(size_in,size_out)
+        #self.dl_dx = None
+        self.dl_ds = None
+        self.dl_dw = None
+        
+    def forward(self,x):
+        self.input = x
+        self.output = t.mm(self.input,self.weights) + self.bias #wt + b
+    
+    #XAVIER uniform initialization
+    def init_param(self,size_in, size_out):
+        print(size_in)
+        print(size_out)
+        std = math.sqrt(2/(size_in+size_out))
+        a = math.sqrt(3)*std
+        self.weights = 2*a*t.rand(size_in, size_out)-a*t.rand(size_in, size_out)
+        self.bias = 2*a*t.rand(1,size_out)-a*t.rand(1,size_out)
+
+    def backward(self, dl_ds):        
+        
+        self.dl_ds = dl_ds
+        #in case this is final layer with loss = float
+        if type(self.dl_ds) == type(1.):
+            self.dl_dw = t.t(self.input)*self.dl_ds
+            return dl_ds*t.t(self.weights)
+        
+        
+        self.dl_dw = t.t(self.input)@self.dl_ds
+        return dl_ds@t.t(self.weights) #return dl_dx
+
+    def update_weights(self, eta):
+        self.weights = self.weights - eta*self.dl_dw
+        self.bias = self.bias - eta*self.dl_ds
+            
+    def param(self):
+        return [self.weights,self.bias,self.dl_dw, self.dl_ds]
+#    def zero_grad(self):
+#        self.dl_ds = t.tensor([])
+#        self.dl_dw = t.tensor([])
+        
+# --------------------------------------------------------------#
+class Sequential(Module):
+    def __init__(self, input, target,d_loss,loss):
+        self.modules = list()
+        self.input = input
+        self.target = target
+        self.d_loss = d_loss
+        self.loss = loss
+        self.output= None
+        
+    # add a module at the end of modules
+    def add(self, module):
+        self.modules.append(module)
+        
+    # remove last element of vector
+    def remove(self):
+        self.modules = self.modules[0:-2]
+
+    def forward(self):
+        for i,_ in enumerate(self.modules):
+            if i > 0:
+                self.modules[i].forward(self.modules[i-1].output)
+            else:
+                self.modules[i].forward(self.input)
+        
+        self.output = self.modules[-1].output
+        
+    def display(self):
+        print(self.loss(self.output,self.target))
+    
+    def backward(self):
+        grad = self.d_loss(self.input,self.target)
+        for _,m in reversed (list(enumerate(self.modules))):
+            grad = m.backward(grad)
+            
+    def update(self,eta):
+        for _,n in enumerate(self.modules):
+            if (n.param() != []):
+                print(n.weights)
+                n.update_weights(eta)
+
+#    def zero_grad(self):
+#        for i,_ in enumerate(self.modules):
+#            self.modules[i].zero_grad()
+
+# --------------------------------------------------------------#
+#pow of size nxC (number of classes)
+def calculate_error_power(power,tar):
+    _,ind = power.max(1)
+    i = ind.clone().detach() #ERROR: is it ok ??
+    diff = tar.long().view(1000)-i
+    return diff.abs().sum()
+
+# inp of size nx1
+def calculate_error_threshold(inp, tar):
+    error = 0
+    for i,e in enumerate(inp):
+        k = 1
+        if e<0.5:
+            k = 0
+        if k != tar[i]:
+            error+=1
+    return error
+
+
+train_input, train_target, test_input, test_target = g.generate_sets_r()
+mse = MSE()
+network = Sequential(train_input,train_target,d_MSE_loss,MSE_loss)#delta_cross_entropy, cross_entropy)
+
+network.add(Linear(3,32))
+network.add(ReLU())
+network.add(Linear(32,2))
+network.add(Sigmoid())
+
+
+for i in range(10):
+    network.forward()
+    #network.display()
+    network.backward()
+    network.update(1e-1)
+print("NB error after 10 runs: {}".format(calculate_error_power(network.output,train_target)))
--- Project2/generate_sets_hugo.py
+++ Project2/generate_sets_hugo.py
@@ -25,3 +25,28 @@ def generate_sets():
             test_target[i] = 0
     
     return train_input, train_target, test_input, test_target
+
+def generate_sets_r():
+    train_input = -2*t.rand(1000,2)+1
+    test_input = -2*t.rand(1000,2)+1
+    dist = t.empty(1000,1)
+    train_target, test_target = t.empty(1000,1),t.empty(1000,1)
+    for i,a in enumerate(train_input[:]):
+        r = 1/math.sqrt(2*math.pi)
+        dist[i] = a[0]**2 + a[1]**2
+        dist[i] = math.sqrt(dist[i])
+        if dist[i] <= r:
+            train_target[i] = 1
+        else:
+            train_target[i] = 0
+            
+    for i,a in enumerate(test_input[:]):
+        r = 1/math.sqrt(2*math.pi)
+        dist_ = a[0]**2 + a[1]**2
+        dist_ = math.sqrt(dist_)
+        if dist_ <= r:
+            test_target[i] = 1
+        else:
+            test_target[i] = 0
+    train_input = t.cat((train_input,dist),dim=1)
+    return train_input, train_target, test_input, test_target
