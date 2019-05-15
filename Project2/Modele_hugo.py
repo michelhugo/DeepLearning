66aa7cc4f7d95d88974a0c2376aa5a654c412b15
Hugo Michel <hugo.michel@epfl.ch>
2019-05-15 10:18:00 +0200

    Update Modele_hugo.py

--- Project2/Modele_hugo.py
+++ Project2/Modele_hugo.py
@@ -41,6 +41,23 @@ def d_MSE_loss(predictions, target):
     #return (2*(predictions-target)).mean().item()
     return 2/target.size(0)*(predictions-target)
 
+def one_hot(labels, nb_labels):
+    '''
+    input labels can be either np array or tensors
+    output is a torch.tensor
+    '''
+    if (type(labels) == np.ndarray):
+        h_labels = (np.arange(nb_labels) == labels[:, None]).astype(np.float32)
+
+    elif (type(labels) == t.Tensor):
+        h_labels = labels.numpy()
+        h_labels = (np.arange(nb_labels) == h_labels[:, None]).astype(np.float32)
+        
+    else:
+        raise ValueError('The input type must be either numpy.ndarray or torch.Tensor')
+    
+    return t.Tensor(h_labels).view(labels.size(0),nb_labels)
+    
 # --------------------------------------------------------------#
 class optimizer_SGD:
     def __init__(self, network):
@@ -58,6 +75,36 @@ class optimizer_SGD:
                 self.v[i] = self.v[i]*gamma + n.dl_ds*eta
                 n.weights = n.weights - self.u[i]
                 n.bias = n.bias - self.v[i]
+                
+class optimizer_Adam:
+    def __init__(self, network):
+        self.m_w = {}
+        self.v_w = {}
+        self.m_b = {}
+        self.v_b = {} 
+        for i,n in enumerate(network.modules):
+            if (n.param() != []):
+                self.m_w[i] = 0
+                self.v_w[i] = 0
+                self.m_b[i] = 0
+                self.v_b[i] = 0
+                
+    def step(self, sequential, eta, epsilon, beta1, beta2):
+        for i,n in enumerate(sequential.modules):
+            if (n.param() != []):
+                self.m_w[i] = self.m_w[i]*beta1 + (1-beta1)*n.dl_dw
+                self.m_b[i] = self.m_b[i]*beta1 + (1-beta1)*n.dl_ds
+                self.v_w[i] = self.v_w[i]*beta2 + (1-beta2)*n.dl_dw**2
+                self.v_b[i] = self.v_b[i]*beta2 + (1-beta2)*n.dl_ds**2
+                
+                m_w_t  = self.m_w[i]/(1-beta1)
+                m_b_t  = self.m_b[i]/(1-beta1)
+                
+                v_w_t = self.v_w[i]/(1-beta2)
+                v_b_t = self.v_b[i]/(1-beta2)
+                
+                n.weights = n.weights - eta*m_w_t/(v_w_t**(0.5)+epsilon)
+                n.bias = n.bias - eta*m_b_t/(v_b_t**(0.5)+epsilon)
 # --------------------------------------------------------------#
 class Module ():        
     
@@ -92,7 +139,7 @@ class ReLU(Module):
         
     def forward(self, s):
         self.input = s
-        self.output = self.activation(s)
+        return self.activation(s)
         
     def backward(self, dl_dx):
         return dl_dx*self.d_activation(self.input)
@@ -106,7 +153,7 @@ class tanh(Module):
         
     def forward(self, s):
         self.input = s
-        self.output = self.activation(s)
+        return self.activation(s)
         
     def backward(self, dl_dx): 
        return dl_dx*self.d_activation(self.input)
@@ -120,7 +167,7 @@ class Sigmoid(Module):
         
     def forward(self, s):
         self.input = s
-        self.output = self.activation(s)
+        return self.activation(s)
         
     def backward(self, dl_dx): 
        return dl_dx*self.d_activation(self.input)
@@ -134,7 +181,7 @@ class Linear(Module):
         
     def forward(self,x):
         self.input = x
-        self.output = t.mm(self.input,self.weights) + self.bias #wt + b
+        return t.mm(self.input,self.weights) + self.bias #wt + b
     
     #XAVIER uniform initialization
     def init_param(self,size_in, size_out):
@@ -154,7 +201,26 @@ class Linear(Module):
             
     def param(self):
         return [self.weights,self.bias,self.dl_dw, self.dl_ds]
-
+# --------------------------------------------------------------#
+class Dropout(Module):
+    def __init__(self, p, input_size, seed=0):
+        self.p = p
+        self.generator = np.random.RandomState(seed)
+        self.activation = self.generator.binomial(size=input_size, n=1, p=1-p)
+        self.activation = t.from_numpy(self.activation).float()
+        self.training = True
+        
+    def set_training(self, b):
+        self.training = b;
+        
+    def forward(self, input):
+        if self.training:
+            return input*self.activation
+        else:
+            return input
+    
+    def backward(self, grad):
+        return grad
 # --------------------------------------------------------------#
 class Sequential(Module):
     def __init__(self, target,d_loss,loss):
@@ -173,13 +239,11 @@ class Sequential(Module):
         self.modules = self.modules[0:-2]
 
     def forward(self, input):
+        next_in = input
         for i,_ in enumerate(self.modules):
-            if i > 0:
-                self.modules[i].forward(self.modules[i-1].output)
-            else:
-                self.modules[i].forward(input)
+                next_in = self.modules[i].forward(next_in)
         
-        self.output = self.modules[-1].output
+        self.output = next_in
         
     def display(self):
         print(self.loss(self.output,self.target))
@@ -199,7 +263,7 @@ class Sequential(Module):
 def calculate_error_power(power,tar):
     _,ind = power.max(1)
     i = ind.clone().detach()
-    diff = tar.long().view(1000)-i
+    diff = tar.long().view(tar.size(0))-i
     return diff.abs().sum()
 
 # inp of size nx1
@@ -213,27 +277,51 @@ def calculate_error_threshold(inp, tar):
             error+=1
     return error
 
+#test_labels,train_labels = target_to_one_hot(test_target),target_to_one_hot(train_target)
 
 train_input, train_target, test_input, test_target = g.generate_sets()
-network = Sequential(train_target,d_MSE_loss, MSE_loss)
+train_labels = one_hot(train_target,2)
+network = Sequential(train_labels, d_MSE_loss, MSE_loss)
 
-network.add(Linear(2,8))
+
+#network.add(Linear(2,8))
+#network.add(Dropout(0.5,8))
+#network.add(ReLU())
+#network.add(Linear(8,16))
+#network.add(Dropout(0.5,16))
+#network.add(ReLU())
+#network.add(Linear(16,1))
+#network.add(Sigmoid())
+
+network.add(Linear(2,25))
+network.add(Dropout(0.5,25))
 network.add(ReLU())
-network.add(Linear(8,1))
+network.add(Linear(25,25))
+network.add(Dropout(0.5,25))
+network.add(ReLU())
+network.add(Linear(25,25))
+network.add(Dropout(0.5,25))
+network.add(Sigmoid())
+network.add(Linear(25,2))
 network.add(Sigmoid())
 SGD = optimizer_SGD(network)
+Adam = optimizer_Adam(network)
 
-for i in range(2000):
+for i in range(1000):
     network.forward(train_input)
     network.display()
     network.backward()
     
-    SGD.step(network, 5e-2, gamma = 0.9)
-    #network.update(5e-2)
+    #SGD.step(network, 1e-2, gamma = 0.5)
+    #network.update(5e-1)
+    Adam.step(network, 5e-3, 1e-3, 0.5, 0.6)
 
-print("NB error for input: {}".format(calculate_error_threshold(network.output,train_target)))
+print("NB error for input: {}".format(calculate_error_power(network.output,train_target)))
 print(train_target.sum())
 
+network.modules[1].set_training(False)
+network.modules[4].set_training(False)
+network.modules[7].set_training(False)
 network.forward(test_input)
-print("NB error for test: {}".format(calculate_error_threshold(network.output,test_target)))
+print("NB error for test: {}".format(calculate_error_power(network.output,test_target)))
 print(test_target.sum())
